47853e13b86787de6679b7970fde6695cf16afe1
leeool <lgonsalez02@gmail.com>
2022-05-20 22:16:06 -0300

    finaliza

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int n1, n2, sequencia;
+
+Console.Write("Digite o primeiro numero: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo numero: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+sequencia = n1;
+
+while (sequencia != n2){
+    sequencia = n1++;
+    Console.Write($"{n1-1} ");
+}
+
+Console.Write("\n");
